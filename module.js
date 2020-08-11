b39bb599834362dfe9cd5abb48ad67822b7fffd1
David Andersson <lejonmanen@gmail.com>
2020-08-11 14:45:32 +0200

    Import files as modules in Node

---
+++ module.js
@@ -0,0 +1,7 @@
+function moduleFunction() {
+    console.log('This is the function in the module.');
+}
+
+module.exports = moduleFunction;
+
+// i React: export default moduleFunction
--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const importedFunction = require('./module.js');
+const fs = require('fs');
+
+// i React: import moduleFunction from './module.js'
+
 console.log('Hello world!');
+
+importedFunction();
+
+fs.readFile('./fakeDatabase.txt', (error, data) => {
+    if( error ) {
+        console.log('Error when reading the file: ' + error);
+        return;
+    }
+    console.log('Read from file: ' + data);
+    // JSON.parse if we want to convert it to a JavaScript data type
+})
