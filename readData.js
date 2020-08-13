cd21195f7f275c687d73406797f5b0dfc36a2a57
David Andersson <lejonmanen@gmail.com>
2020-08-13 11:28:22 +0200

    Exercise 1.5b

--- readData.js
+++ readData.js
@@ -1,10 +1,25 @@
 const fs = require('fs')
 
-fs.readFile('./personData.json', (error, data) => {
-    if( error ) {
-        console.log('Something went wrong (personData) ', error);
-        return;
-    }
+// 1.5a
+// fs.readFile('./personData.json', (error, data) => {
+//     if( error ) {
+//         console.log('Something went wrong (personData) ', error);
+//         return;
+//     }
+//     // nu har vi filens text-innehåll
+//     let object = JSON.parse(data);
+//     console.log(`Name: ${object.name}, email: ${object.email}`);
+// })
+
+// 1.5b
+let readStream = fs.createReadStream('./personData.json');
+
+let data = '';
+readStream.on('data', chunk => {
+    console.log('Received package from stream: ' + chunk.toString());
+    data += chunk;
+})
+readStream.on('end', () => {
     // nu har vi filens text-innehåll
     let object = JSON.parse(data);
     console.log(`Name: ${object.name}, email: ${object.email}`);
