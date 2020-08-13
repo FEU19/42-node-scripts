4fa7d0f84669039cacc87715411ac3149a3e88cd
David Andersson <lejonmanen@gmail.com>
2020-08-13 10:48:36 +0200

    Exercise 1.4

--- script.js
+++ script.js
@@ -17,3 +17,30 @@ fs.readFile('./fakeDatabase.txt', (error, data) => {
     console.log('Read from file: ' + data);
     // JSON.parse if we want to convert it to a JavaScript data type
 })
+
+
+// Uppgift: hämta och ändra datan.
+// Namnet är felstavat, ska vara "Johan" och "johan@home.se"
+// 1. läs innehållet i filen
+// 2. omvandla texten till ett JavaScript-objekt (mha JSON.parse)
+// 3. göra ändringar i objektet
+// 4. omvandla objektet till en sträng (mha JSON.stringify)
+// 5. spara strängen till filen
+fs.readFile('./personData.json', (error, data) => {
+    if( error ) {
+        console.log('Something went wrong (personData) ', error);
+        return;
+    }
+    // nu har vi filens text-innehåll
+    let object = JSON.parse(data);
+    console.log('Reading personData', object);
+
+    // dags att göra ändringarna
+    object.name = 'Johan';
+    object.email = 'johan@home.se';
+
+    let newText = JSON.stringify(object);
+    fs.writeFile('./personData.json', newText, () => {
+        console.log('Saved new person data to file');
+    })
+})
